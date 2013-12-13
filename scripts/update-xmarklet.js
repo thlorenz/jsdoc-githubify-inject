1ae5482272f42c4dde9a4d66f99a69df1cd74ec6
Thorsten Lorenz <thlorenz@gmx.de>
2013-12-13 12:40:16 -0500

    generating browserified xmarklet

--- scripts/update-xmarklet.js
+++ scripts/update-xmarklet.js
@@ -2,8 +2,25 @@
 
 var bookmarkletify = require('bookmarkletify')
   , fs = require('fs')
+  , browserify = require('browserify');
 
-var source = fs.readFileSync(__dirname + '/../inject.js', 'utf8');
+var source = fs.readFileSync(__dirname + '/../inject.js', 'utf8')
+  //, html = fs.readFileSync(__dirname + '/../index.html', 'utf8')
 
-var xmarkjs = bookmarkletify(source)
-fs.writeFileSync(__dirname + '/../jsdoc-githubify-inject.xmarklet.js', xmarkjs, 'utf8');
+browserify(__dirname + '/../').bundle(function (err, bundled) {
+  var xmarkified = bookmarkletify(bundled);
+  
+  var xmarkhtml = [
+      '<!-- START XMARKLET -->'
+    , '<a href="' + xmarkified + '"'
+    , '   onclick="window.alert(\'Right click on a bookmarklet and select Add to Favorites...\');return false;"'
+    , '   class="xmarklet-link">'
+    , '   <strong>jsdoc-githubify-inject</strong>'
+    , '   bookmarklet'
+    , '</a>'
+    , '<!-- END XMARKLET -->'
+  ].join('\n')
+  // TODO: update section
+  console.log(xmarkhtml);
+});
+
