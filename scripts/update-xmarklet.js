c3488ae2e919ade4a5e633d12dca1241b551a351
Thorsten Lorenz <thlorenz@gmx.de>
2013-12-13 12:45:58 -0500

    update xmarklet script now updates index.html automatically

--- scripts/update-xmarklet.js
+++ scripts/update-xmarklet.js
@@ -2,10 +2,11 @@
 
 var bookmarkletify = require('bookmarkletify')
   , fs = require('fs')
-  , browserify = require('browserify');
+  , browserify = require('browserify')
+  , updateSection = require('update-section');
 
 var source = fs.readFileSync(__dirname + '/../inject.js', 'utf8')
-  //, html = fs.readFileSync(__dirname + '/../index.html', 'utf8')
+  , html = fs.readFileSync(__dirname + '/../index.html', 'utf8')
 
 browserify(__dirname + '/../').bundle(function (err, bundled) {
   var xmarkified = bookmarkletify(bundled);
@@ -20,7 +21,13 @@ browserify(__dirname + '/../').bundle(function (err, bundled) {
     , '</a>'
     , '<!-- END XMARKLET -->'
   ].join('\n')
-  // TODO: update section
-  console.log(xmarkhtml);
+
+  var updatedHtml = updateSection(
+      html
+    , xmarkhtml
+    , function start(s) { return (/<!-- START XMARKLET/).test(s) }
+    , function end(s) { return (/<!-- END XMARKLET/).test(s) }
+  )
+  fs.writeFileSync(__dirname + '/../index.html', updatedHtml, 'utf8');
 });
 
