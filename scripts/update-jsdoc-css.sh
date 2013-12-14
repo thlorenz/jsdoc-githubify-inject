bac116b0292158aa0a7109bc4753307035edfe4d
Thorsten Lorenz <thlorenz@gmx.de>
2013-12-13 22:50:47 -0500

    update css script now namespaces

--- scripts/update-jsdoc-css.sh
+++ scripts/update-jsdoc-css.sh
@@ -1,1 +1,3 @@
-curl -L https://raw.github.com/jsdoc3/jsdoc/master/templates/default/static/styles/jsdoc-default.css > jsdoc-default.css
+curl -L https://raw.github.com/jsdoc3/jsdoc/master/templates/default/static/styles/jsdoc-default.css \
+  | ./node_modules/.bin/namespace-css --selector .jsdoc-githubify \
+  > jsdoc-default.css
