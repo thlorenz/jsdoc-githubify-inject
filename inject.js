e2ccad34624132378e5d06db604624f13be2c73c
Thorsten Lorenz <thlorenz@gmx.de>
2013-12-14 11:39:12 -0500

    adding widths css and regenerating chrome extension

--- inject.js
+++ inject.js
@@ -1,11 +1,14 @@
 'use strict';
 
 var githubifieds = document.getElementsByClassName('jsdoc-githubify');
-var css = require('fs').readFileSync(__dirname + '/jsdoc-default.css');
+var css = require('fs').readFileSync(__dirname + '/jsdoc-default.css')
+
+  // make wiki page a bit wider (980px -> 1200px and 920px -> 1155px) in order to render docs better
+  , widthsCss = '.wrapper .container { width: 1200px; }\n.wrapper .repository-with-sidebar .repository-content { width: 1155px; }\n'
 
 if (githubifieds.length) {
   var style = document.createElement('style');
-  style.textContent = css;
+  style.textContent = widthsCss + css;
 
   var first = document.head.firstChild;
   document.head.insertBefore(style, first);
