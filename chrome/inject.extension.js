e3470281761a181dffcc40a9317d7adccbdee896
Thorsten Lorenz <thlorenz@gmx.de>
2013-12-13 23:03:18 -0500

    improvements to chrome extension

--- chrome/inject.extension.js
+++ chrome/inject.extension.js
@@ -1,5 +1,5 @@
 'use strict';
 
 var script = document.createElement('script');
-script.src = 'https://github.com/thlorenz'; 
+script.src = 'https://raw.github.com/thlorenz/jsdoc-githubify-inject/master/chrome/extension.js'; 
 document.querySelector('script').parentNode.insertBefore(script);
